1692e7cefeb6e86ce43f1591057c17c01a298137
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-04 11:24:49 -0300

    update: css styles added

--- src/Principal.js
+++ src/Principal.js
@@ -1,6 +1,6 @@
 import Pessoa from "./Pessoa.js";
-import './assets/style.css'
-
+import './assets/css/style.css'
+import './assets/css/style.scss'
 const atendente = new Pessoa();
 
 atendente.cumprimentar()
